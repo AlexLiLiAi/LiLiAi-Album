0bc98051dfb64768ded323bb89b05397c278db4f
AlexLiLiAi <bawbhv@gmail.com>
2026-09-07 16:45:52 +0200

    Add files via upload

---
+++ app/src/main/java/com/lilialbum/player/MainActivity.java
@@ -0,0 +1,39 @@
+package com.lilialbum.player;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+    private ImageView splash;
+
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(1024, 1024);
+        setContentView(R.layout.activity_main);
+        splash = findViewById(R.id.splash);
+        webView = findViewById(R.id.webview);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(true);
+        s.setMediaPlaybackRequiresUserGesture(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("file:///android_asset/index.html");
+        new Handler().postDelayed(() -> splash.setVisibility(View.GONE), 1600);
+    }
+
+    @Override public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
